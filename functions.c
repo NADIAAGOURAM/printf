83dd4c3e81e18c3c0a27498464bfa67838b6e21a
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-05-27 16:44:23 -0700

    C - printf

---
+++ functions.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _putchar - writes a character to stdout
+ * @c:caracter
+ * Return:1 on success On error -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ *_strlen - return length of a string
+ *@s: pointer char
+ *Return: int length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ *print_string - print string
+ *@s: pointer char
+ *Return: length string
+ */
+int print_string(char *s)
+{
+	int i;
+
+	if (s == NULL)
+	{
+		s = "(null)";
+	}
+	for (i = 0; i < _strlen(s); i++)
+	{
+		_putchar(s[i]);
+	}
+	return (_strlen(s));
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int _strlen(char *s);
+int print_string(char *s);
+
+
+#endif
