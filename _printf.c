a3144c4148608052681b5f672ed20f847ffb3269
slimouch <saloua.sarkadi@gmail.com>
2023-05-28 07:17:38 -0700

    _printf.c updated

--- _printf.c
+++ _printf.c
@@ -1,3 +1,5 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "main.h"
 /**
 * _printf - produces output according to a format.
@@ -7,26 +9,24 @@
 */
 int _printf(const char *format, ...)
 {
-	va_list = argl;
+	va_list argl;
 	int somme = 0;
-	char *db, *arg;
-	params_t ArgIni = PARAMS_INIT;
+	char *arg;
 
 	va_start(argl, format);
+
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (!format || (format[0] == '%' && format[1] == ' ' && !format[2]))
 		return (-1);
 	for (arg = (char *)format; *arg; arg++)
 	{
-		init_params(&ArgInit, argl);
 		if (*arg != '%')
 		{	somme += _putchar(*arg);
 			continue;
 		}
-		db = p;
-		p++;
-		while (format[1] == 'c' || format[1] == 's' == format[1] == '%')
+		arg++;
+		while (format[1] == 'c' || format[1] == 's' || format[1] == '%')
 		{
 			_putchar(*arg);
 		}
