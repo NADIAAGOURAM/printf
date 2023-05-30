4b46145d17f76b63dcf320bc6b70d163a7986f16
slimouch <128979583+slimouch@users.noreply.github.com>
2023-05-30 17:28:23 +0100

    Update _printf.c1

--- _printf.c
+++ _printf.c
@@ -7,15 +7,15 @@
 */
 int _printf(const char *format, ...)
 {
-	va_list argl;
-	int somme = 0;
+    va_list argl;
+    int somme = 0;
 
-	va_start(argl, format);
-	if (!format || (*format == '%' && !*format++))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	somme += print_cases(format, argl);
-	va_end(argl);
-	return (somme);
+    va_start(argl, format);
+    if (!format || (format[0] == '%' && !format[1]))
+        return (-1);
+    if (format[0] == '%' && format[1] == ' ' && !format[2])
+        return (-1);
+    somme += print_cases(format, argl);
+    va_end(argl);
+    return (somme);
 }
