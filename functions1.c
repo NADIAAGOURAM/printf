cbad75543b027cce0386fb7ef9fab2ba317d9459
slimouch <saloua.sarkadi@gmail.com>
2023-05-30 13:02:55 -0700

    functions1

--- functions1.c
+++ functions1.c
@@ -1,9 +1,31 @@
 #include "main.h"
+
 /**
- * printUnsigned - print Unsigned
- * @number: unsigned int
+ * print_binary - prints binary
+ * @b:unsigned int
  * Return: length number
- */
+ **/
+int print_binary(unsigned int b)
+{
+	int i, count = 0, bits[32], index = 0;
+
+	if (b == 0)
+		return (_putchar('0'));
+	while (b > 0)
+	{
+		bits[index++] = b % 2;
+		b /= 2;
+	}
+	for (i = index - 1; i >= 0; i--)
+		count += _putchar('0' + bits[i]);
+	return (count);
+}
+
+/**
+  * printUnsigned - print Unsigned
+  * @number:unsigned int
+  * Return: length number
+ **/
 int printUnsigned(unsigned int number)
 {
 	int count = 0;
